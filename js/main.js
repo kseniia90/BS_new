0bbf416cb307c420015774054786b2c31eace16c
Kseniia <xenio310390@gmail.com>
2025-09-17 17:07:01 +0300

    progress

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 "use strict";
 // menu start
 
+const mobileBreakpoint = 1400;
 const burgerMenuIcon = document.querySelector(".burger-menu__icon");
 const burgerMenuBody = document.querySelector(".header__nav-container");
 
 document.addEventListener("click", function (event) {
   if (event.target.closest(".burger-menu-btn")) {
     document.body.classList.toggle("lock");
-    burgerMenuBody.classList.toggle("_active");
-    burgerMenuIcon.classList.toggle("_active");
+    burgerMenuBody.classList.toggle("active");
+    burgerMenuIcon.classList.toggle("active");
   }
 });
 
@@ -18,7 +19,7 @@ let dropdownCloseTimeout;
 
 // Dropdown Open and Close function START
 function dropDownFunc(dropDown) {
-  if (window.innerWidth > 900) {
+  if (window.innerWidth >= mobileBreakpoint) {
     if (dropDown.classList.contains("hover-dropdown") === true) {
       dropDown.onmouseover = dropDown.onmouseout = dropdownHover;
 
@@ -30,7 +31,6 @@ function dropDownFunc(dropDown) {
           // add the open and active class(Opening the DropDown)
           this.parentElement.classList.add("dropdown-open");
           this.nextElementSibling.classList.add("dropdown-active");
-        //   this.querySelector(".arrow_down").classList.add("rotate");
         }
       }
     }
@@ -73,9 +73,7 @@ function dropDownFunc(dropDown) {
 
           // add the open and active class(Opening the DropDown)
           this.parentElement.classList.add("dropdown-open");
-        //   if (!!this.querySelector(".arrow_down")) {
-        //     this.querySelector(".arrow_down").classList.add("rotate");
-        //   }
+
           if (!!this.nextElementSibling) {
             this.nextElementSibling.classList.add("dropdown-active");
 
@@ -93,7 +91,7 @@ function dropDownFunc(dropDown) {
 }
 
 document.querySelectorAll(".header__nav-link > a").forEach(function (dropDown) {
-  if (window.innerWidth > 900) {
+  if (window.innerWidth >= mobileBreakpoint) {
     if (dropDown.classList.contains("hover-dropdown") !== true) {
       dropDown.onmouseover = dropDown.onmouseout = dropdownHover;
 
@@ -108,7 +106,7 @@ document.querySelectorAll(".header__nav-link > a").forEach(function (dropDown) {
 
 // Listen to the doc click
 window.addEventListener("click", function (e) {
-  if (window.innerWidth > 900) {
+  if (window.innerWidth >= mobileBreakpoint) {
     // Close the menu if click happen outside menu
     if (!e.target.closest(".header__nav__list")) {
       // Close the opend dropdown
@@ -118,8 +116,8 @@ window.addEventListener("click", function (e) {
     // close burger menu
     if (!e.target.closest(".header__nav-container") &&!e.target.classList.contains("burger-menu__icon")) {
       document.body.classList.remove("lock");
-      burgerMenuBody.classList.remove("_active");
-      burgerMenuIcon.classList.remove("_active");
+      burgerMenuBody.classList.remove("active");
+      burgerMenuIcon.classList.remove("active");
 
       this.setTimeout(function () {
         $(".header__second-submenu__list.open").removeClass("open");
@@ -129,7 +127,7 @@ window.addEventListener("click", function (e) {
       }, 400);
     }
 
-    if (e.target.closest(".header__nav__back")) {
+    if (e.target.closest(".nav__close")) {
       if ($(".header__second-submenu__list.open").length) {
         $(".header__second-submenu__list.open").removeClass("open");
       } else if ($(".header__nav-link.dropdown-open").length) {
@@ -138,8 +136,8 @@ window.addEventListener("click", function (e) {
         $(".header__nav-link.dropdown-open").removeClass("dropdown-open");
       } else {
         document.body.classList.remove("lock");
-        burgerMenuBody.classList.remove("_active");
-        burgerMenuIcon.classList.remove("_active");
+        burgerMenuBody.classList.remove("active");
+        burgerMenuIcon.classList.remove("active");
       }
     }
   }
@@ -156,9 +154,6 @@ function closeDropdown() {
     menu.classList.remove("dropdown-active");
   });
 
-//   document.querySelectorAll(".arrow_down").forEach(function (container) {
-//     container.classList.remove("rotate");
-//   });
 }
 // dropdown menu END
 
@@ -167,16 +162,16 @@ $(".has-submenu > a").click(function (e) {
   e.preventDefault();
   if (!$(this).siblings(".header__second-submenu__list").hasClass("open")) {
     $(".header__second-submenu__list").removeClass("open");
-    if (window.innerWidth > 900) {
+    if (window.innerWidth >= mobileBreakpoint) {
       $(".header__second-submenu__list").slideUp();
     }
     $(this).siblings(".header__second-submenu__list").addClass("open");
-    if (window.innerWidth > 900) {
+    if (window.innerWidth >= mobileBreakpoint) {
       $(this).siblings(".header__second-submenu__list").slideDown();
     }
   } else {
     $(this).siblings(".header__second-submenu__list").removeClass("open");
-    if (window.innerWidth > 900) {
+    if (window.innerWidth >= mobileBreakpoint) {
       $(this).siblings(".header__second-submenu__list").slideUp();
     }
   }

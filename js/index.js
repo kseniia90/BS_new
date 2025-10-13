b1bccdc4f0dbcc256e9f2b4d08ebc4eda6c4b9f8
Kseniia Pukhova <xenio310390@gmail.com>
2025-10-13 19:50:20 +0300

    post page progress

--- js/index.js
+++ js/index.js
@@ -20,3 +20,22 @@ const swiperPhotos = new Swiper('.photo-slider', {
     el: '.swiper-pagination',
   }
 });
+
+
+document.querySelectorAll('.scroll-box').forEach(function (scrollBox) {
+  const container = scrollBox;
+  const wrapper = document.createElement("div");
+  wrapper.className = "scroll-box-wrapper";
+  container.parentNode.insertBefore(wrapper, container);
+  wrapper.appendChild(container);
+
+  wrapper.style.setProperty("--scroll-box-gap", window.getComputedStyle(scrollBox).columnGap);
+
+  const items = Array.from(wrapper.children);
+  items.forEach(item => {
+    const clone = item.cloneNode(true);
+    wrapper.appendChild(clone);
+  });
+
+  wrapper.classList.add("animate");
+});
